cd6165e86adfc5cfa80f69dc055da4105057ba17
dorkpad <creek.frogs@gmail.com>
2026-06-01 15:28:59 -0700

    Basic rendering loop in main.c

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include "SDL_render.h"
 #include <SDL2/SDL.h>
 #define WINDOW_W 480
 #define WINDOW_H 320
@@ -18,14 +19,33 @@ int main(void)
 
     SDL_Renderer *renderer = SDL_CreateRenderer(
         window, -1,
-        SDL_RENDERER_ACCELERATED);
+        SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
-    SDL_SetRenderDrawColor(renderer, 10, 10, 20, 255);
-    SDL_RenderClear(renderer);
-    SDL_RenderPresent(renderer);
+    int running = 1;
+    SDL_Event event;
 
-    SDL_Delay(3000);
+    while (running)
+    {
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT)
+                running = 0;
+            if (event.type == SDL_KEYDOWN)
+            {
+                if (event.key.keysym.sym == SDLK_ESCAPE)
+                    running = 0;
+            }
+        }
 
+        SDL_SetRenderDrawColor(renderer, 10, 10, 20, 255);
+        SDL_RenderClear(renderer);
+
+        SDL_SetRenderDrawColor(renderer, 100, 50, 200, 255);
+        SDL_Rect rect = {20, 20, 100, 50};
+        SDL_RenderFillRect(renderer, &rect);
+
+        SDL_RenderPresent(renderer);
+    }
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
