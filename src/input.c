b11542137491e30d44af520cf26a84cae2a4137f
dorkpad <creek.frogs@gmail.com>
2026-06-01 19:49:15 -0700

    input switching menus

--- src/input.c
+++ src/input.c
@@ -14,6 +14,18 @@ void input_handle(SDL_Event *event, AppState *state)
         case SDLK_ESCAPE:
             state->running = 0;
             break;
+        case SDLK_1:
+            state->current_menu = MENU_HOME;
+            break;
+        case SDLK_2:
+            state->current_menu = MENU_MUSIC;
+            break;
+        case SDLK_3:
+            state->current_menu = MENU_ROMS;
+            break;
+        case SDLK_4:
+            state->current_menu = MENU_CMDS;
+            break;
         default:
             break;
         }
